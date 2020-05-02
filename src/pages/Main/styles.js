8c116cbb9fb349978d324fbcba7ddc5d465eb534
Juan Mello <juanribeirofx@gmail.com>
2020-05-02 18:35:16 -0300

    Listando os repositorios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -72,3 +72,25 @@ export const SubmitButton = styled.button.attrs((props) => ({
       }
     `}
 `;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #f7a822;
+      text-decoration: none;
+    }
+  }
+`;
